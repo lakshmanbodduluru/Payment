2f34d8f5ec4390affa6939e316e234d47468a3be
lakshman bodduluru <lakshman.bodduluru@gmail.com>
2016-07-01 10:30:50 +0530

    git

--- app/src/main/java/admi/payment/GitHub.java
+++ app/src/main/java/admi/payment/GitHub.java
@@ -2,12 +2,23 @@ package admi.payment;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class GitHub extends AppCompatActivity {
+    Button click;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_git_hub);
+        click = (Button) findViewById(R.id.git_click);
+        click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(GitHub.this, "Success", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
